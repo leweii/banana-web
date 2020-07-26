eaec40a37c29495fa5527bc78aec918a02083d5c
Jakob He <jakob.he@ringcentral.com>
2020-07-26 10:40:40 +0800

    first commit

---
+++ src/test/java/banana/leweii/banana/BananaApplicationTests.java
@@ -0,0 +1,13 @@
+package banana.leweii.banana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BananaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
